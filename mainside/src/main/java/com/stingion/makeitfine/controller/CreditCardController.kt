bb9b3b1e87b6d5e6dfc41face3d35aaa41fdc1ed
koresmosto <koresmosto@gmail.com>
2021-10-13 17:35:22 +0300

    Fix IT

--- mainside/src/main/java/com/stingion/makeitfine/controller/CreditCardController.kt
+++ mainside/src/main/java/com/stingion/makeitfine/controller/CreditCardController.kt
@@ -6,16 +6,26 @@
 
 package com.stingion.makeitfine.controller
 
+import com.stingion.makeitfine.data.model.CreditCard
+import com.stingion.makeitfine.data.service.model.CreditCardService
+import io.swagger.annotations.ApiOperation
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/kotlin")
-class KotlinController {
+@RequestMapping("creditCards")
+class CreditCardController(@Autowired val creditCardService: CreditCardService) {
     data class Message(val id: String?, val text: String)
 
     @GetMapping
+    @ApiOperation("Get all creditCards")
+    fun listAllCreditCard(): List<CreditCard?>? {
+        return creditCardService.findAll()
+    }
+
+    @GetMapping("any")
     fun probe(): List<String> = listOf(
             "one",
             "two",
